5dc7e4d453971c593cdd4f6c01c5381d33b7cb1a
Ichimura-riku <riku-ichimura@cybozu.co.jp>
2025-09-24 09:40:47 +0900

    refactor MainActivity to implement SampleScreen with input and action buttons

--- sample/src/main/java/com/example/sample/MainActivity.kt
+++ sample/src/main/java/com/example/sample/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sample.ui.theme.DataStoreCryptoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DataStoreCryptoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SampleScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +37,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SampleScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        TextField(
+            value = "hoge",
+            onValueChange = {},
+            modifier = Modifier
+        )
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+            ) {
+                Text(text = "write")
+            }
+            Button(
+                onClick = {},
+                modifier = Modifier
+            ) {
+                Text(text = "read")
+            }
+        }
+        Text(
+            text = "result",
+        )
+
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DataStoreCryptoTheme {
-        Greeting("Android")
+        SampleScreen()
     }
 }
