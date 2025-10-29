2e59a3deb8a0db17069648328869f72fa3ebf320
Ichimura-riku <riku-ichimura@cybozu.co.jp>
2025-10-29 12:07:27 +0900

    refactor build.gradle

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,17 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.datastore.crypto.android.application) // AndroidApplicationPlugin
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.example.sample"
-    compileSdk = 35
-
     defaultConfig {
         applicationId = "com.example.sample"
-        minSdk = 27
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -27,13 +22,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
@@ -50,13 +39,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.datastore.preferences)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.datastore.crypto.preferences)
 
 }
